abbd9911a2425418e41594938f9df47d1a2bcc9f
leslytausk-eng <leslytausk@gmail.com>
2025-09-20 14:41:52 +0200

    Update Mygame.py

--- Mygame.py
+++ Mygame.py
@@ -6,11 +6,23 @@ HEIGHT = 1080 # Altura de la ventana
 
 TITLE = "El nombre de su juego" # Título de su proyecto de juego
 FPS = 30 # Número de fotogramas por segundo
-
-
+button_play = Actor("btnplay", (320,200))
+mode = "menu"
 
 def draw():
-    screen.clear()
-    screen.blit("menu",(0,0))
+    
+    if mode == "menu":
+        screen.clear()
+        screen.blit("menu", (0,0))
+        button_play.draw()
+    elif mode == "game":
+        screen.clear()
+        screen.blit("bg", (0,0))
+
+def on_mouse_down(pos):
+    global mode
+    if mode == "menu":
+        if button_play.collidepoint(pos):
+            mode = "game"
 
 pgzrun.go()
